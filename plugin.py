b3c19878157f9dd0a2ab239c59b030b1072281ac
Manuel CISSE <cisse.manuel@gmail.com>
2024-02-11 22:17:35 +0100

    Add list of slaves to monitor

--- plugin.py
+++ plugin.py
@@ -21,6 +21,9 @@
         <param field="Port" label="Port" width="100px" required="true" default="502">
             <description><br/>Set the Port (502 by default)</description>
         </param>
+        <param field="Mode1" label="Slaves" width="100px" required="true" default="1">
+            <description><br/>List of slaves to query on the MODBUS bus. When there is a single meter it should be 1. Used to query multiple meters on the same bus. Example: 1,2,3</description>
+        </param>
     </params>
 </plugin>
 """
@@ -38,24 +41,31 @@ class BasePlugin:
     def __init__(self):
         Domoticz.Log("Start PZEM plugin")
         self.pzem = None
+        self.slave = []
 
     def onStart(self):
+        if Parameters['Mode1'] == '':
+            self.slaves = [1]
+        else:
+            self.slaves = list(map(int, Parameters['Mode1'].split(',')))
+        Domoticz.Log('Reading from slaves: ' + ','.join(str(x) for x in self.slaves))
         if Parameters["Address"] == "0.0.0.0" or Parameters["Address"] == "":
             Domoticz.Log("PZEM plugin started, serial port=" + Parameters["SerialPort"])
-            self.pzem = pzem.PZEM004TSerial(Parameters["SerialPort"])
+            self.pzem = pzem.PZEM004TSerial(Parameters["SerialPort"], self.slaves)
         else:
             Domoticz.Log("PZEM plugin started, remote=" + Parameters["Address"] + ":" + Parameters["Port"])
-            self.pzem = pzem.PZEM004TTCP(Parameters["Address"], int(Parameters["Port"]))
+            self.pzem = pzem.PZEM004TTCP(Parameters["Address"], int(Parameters["Port"]), self.slaves)
         self.pzem.reconnect()
 
-        if not self.__UNIT_P1_SMART_METER in Devices:
-            Domoticz.Device(Name="PZEM-004T meter", Type=250, Subtype=1, Unit=self.__UNIT_P1_SMART_METER).Create()
-        if not self.__UNIT_VOLTAGE in Devices:
-            Domoticz.Device(Name="PZEM-004T voltage", TypeName="General", Subtype=8, Unit=self.__UNIT_VOLTAGE).Create()
-        if not self.__UNIT_POWERFACTOR in Devices:
-            Domoticz.Device(Name="PZEM-004T power factor", TypeName="General", Subtype=31, Unit=self.__UNIT_POWERFACTOR).Create()
-        if not self.__UNIT_FREQ in Devices:
-            Domoticz.Device(Name="PZEM-004T frequency", TypeName="General", Subtype=31, Unit=self.__UNIT_FREQ).Create()
+        for slave in self.slaves:
+            if not self.__UNIT_P1_SMART_METER+(slave-1)*4 in Devices:
+                Domoticz.Device(Name="PZEM-004T meter " + str(slave), Type=250, Subtype=1, Unit=self.__UNIT_P1_SMART_METER+(slave-1)*4).Create()
+            if not self.__UNIT_VOLTAGE+(slave-1)*4 in Devices:
+                Domoticz.Device(Name="PZEM-004T voltage " + str(slave), TypeName="General", Subtype=8, Unit=self.__UNIT_VOLTAGE+(slave-1)*4).Create()
+            if not self.__UNIT_POWERFACTOR+(slave-1)*4 in Devices:
+                Domoticz.Device(Name="PZEM-004T power factor " + str(slave), TypeName="General", Subtype=31, Unit=self.__UNIT_POWERFACTOR+(slave-1)*4).Create()
+            if not self.__UNIT_FREQ+(slave-1)*4 in Devices:
+                Domoticz.Device(Name="PZEM-004T frequency " + str(slave), TypeName="General", Subtype=31, Unit=self.__UNIT_FREQ+(slave-1)*4).Create()
 
         # Set plugin heartbeat to 10s
         Domoticz.Heartbeat(10)
@@ -65,22 +75,26 @@ class BasePlugin:
         self.pzem.close()
 
     def onHeartbeat(self):
-        (voltage, current, power, energy, frequency, powerFactor, alarm) = self.pzem.readAll()
-        #Domoticz.Debug('V={:.1f}\tA={:.3f}\tW={:.1f}\tWh={:d}\tHz={:.1f}\tPF={:.2f}'.format(voltage, current, power, energy, frequency, powerFactor))
-        if voltage == 0:
-            return
-        if self.__UNIT_P1_SMART_METER in Devices:
-            sValue = str(energy) + ';0;0;0;' + str(power) + ';0'
-            Devices[self.__UNIT_P1_SMART_METER].Update(nValue = 0, sValue = sValue)
-        if self.__UNIT_VOLTAGE in Devices:
-            sValue = str(voltage)
-            Devices[self.__UNIT_VOLTAGE].Update(nValue = 0, sValue = sValue)
-        if self.__UNIT_POWERFACTOR in Devices:
-            sValue = str(powerFactor)
-            Devices[self.__UNIT_POWERFACTOR].Update(nValue = 0, sValue = sValue)
-        if self.__UNIT_FREQ in Devices:
-            sValue = str(frequency)
-            Devices[self.__UNIT_FREQ].Update(nValue = 0, sValue = sValue)
+        results = self.pzem.readAll()
+
+        for slave in self.slaves:
+            if results[slave]['error'] == "":
+                #Domoticz.Log('slave={:d}\tV={:.1f}\tA={:.3f}\tW={:.1f}\tWh={:d}\tHz={:.1f}\tPF={:.2f}'.format(slave, results[slave]['voltage'], results[slave]['current'], results[slave]['power'], results[slave]['energy'], results[slave]['frequency'], results[slave]['powerFactor']))
+                if self.__UNIT_P1_SMART_METER+(slave-1)*4 in Devices:
+                    sValue = str(results[slave]['energy']) + ';0;0;0;' + str(results[slave]['power']) + ';0'
+                    Devices[self.__UNIT_P1_SMART_METER+(slave-1)*4].Update(nValue = 0, sValue = sValue)
+                if self.__UNIT_VOLTAGE+(slave-1)*4 in Devices:
+                    sValue = str(results[slave]['voltage'])
+                    Devices[self.__UNIT_VOLTAGE+(slave-1)*4].Update(nValue = 0, sValue = sValue)
+                if self.__UNIT_POWERFACTOR+(slave-1)*4 in Devices:
+                    sValue = str(results[slave]['powerFactor'])
+                    Devices[self.__UNIT_POWERFACTOR+(slave-1)*4].Update(nValue = 0, sValue = sValue)
+                if self.__UNIT_FREQ+(slave-1)*4 in Devices:
+                    sValue = str(results[slave]['frequency'])
+                    Devices[self.__UNIT_FREQ+(slave-1)*4].Update(nValue = 0, sValue = sValue)
+            else:
+                Domoticz.Error('slave={:d} error={:s}'.format(slave, results[slave]['error']))
+
 
 global _plugin
 _plugin = BasePlugin()
--- pzem.py
+++ pzem.py
@@ -6,7 +6,8 @@ from pymodbus.client import ModbusTcpClient
 from pymodbus.framer.rtu_framer import ModbusRtuFramer
 
 class PZEM004T:
-    def __init__(self):
+    def __init__(self, slaves):
+        self.slaves = slaves
         self.port_name = ""
         self.modbus_client = None
 
@@ -28,34 +29,41 @@ class PZEM004T:
         self.modbus_client.close()
 
     def readAll(self):
-        tries = 0
-        data = False
-        while tries < 5:
-            try:
-                # Read all registers (10) from PZEM (reading any other number of registers does not work)
-                req = self.modbus_client.read_input_registers(0, 10, slave=1)#, 0xF8)
-                data = req.registers;
-                break
-            except Exception as e:
-                tries += 1
-                print(e)
-            self.reconnect()
-            print("read failed on " + self.port_name + ", trying again " + str(tries))
-        if data == False:
-            return (0, 0, 0, 0, 0, 0, 0)
-        voltage = data[0] / 10.0 # [V]
-        current = (data[1] + (data[2] << 16)) / 1000.0 # [A]
-        power = (data[3] + (data[4] << 16)) / 10.0 # [W]
-        energy = data[5] + (data[6] << 16) # [Wh]
-        frequency = data[7] / 10.0 # [Hz]
-        powerFactor = data[8] / 100.0
-        alarm = data[9] # 0 = no alarm
+        results = {}
+        for current_slave in self.slaves:
+            results[current_slave] = {}
+            data = False
+            tries = 0
+            while tries < 5:
+                try:
+                    # Read all registers (10) from PZEM (reading any other number of registers does not work)
+                    req = self.modbus_client.read_input_registers(0, 10, slave=current_slave)#, 0xF8)
+                    data = req.registers
+                    err = ""
+                    break
+                except Exception as e:
+                    tries += 1
+                    err=str(e)
+                    print(e)
+                self.reconnect()
+                print("read failed on " + self.port_name + "/slave=" + str(current_slave) + ", trying again " + str(tries))
+            if data == False:
+                results[current_slave]['error'] = "read failed on " + self.port_name + "/slave=" + str(current_slave) + ": " + err
+            else:
+                results[current_slave]['error'] = ""
+                results[current_slave]['voltage'] = data[0] / 10.0 # [V]
+                results[current_slave]['current'] = (data[1] + (data[2] << 16)) / 1000.0 # [A]
+                results[current_slave]['power'] = (data[3] + (data[4] << 16)) / 10.0 # [W]
+                results[current_slave]['energy'] = data[5] + (data[6] << 16) # [Wh]
+                results[current_slave]['frequency'] = data[7] / 10.0 # [Hz]
+                results[current_slave]['powerFactor'] = data[8] / 100.0
+                results[current_slave]['alarm'] = data[9] # 0 = no alarm
 
-        return (voltage, current, power, energy, frequency, powerFactor, alarm)
+        return results
 
 class PZEM004TSerial(PZEM004T):
-    def __init__(self, usb_port):
-        PZEM004T.__init__(self)
+    def __init__(self, usb_port, slaves):
+        PZEM004T.__init__(self, slaves)
         self.port_name = usb_port
         self.modbus_client = ModbusSerialClient(
             method='rtu',
@@ -69,7 +77,7 @@ class PZEM004TSerial(PZEM004T):
         )
 
 class PZEM004TTCP(PZEM004T):
-    def __init__(self, host, port):
-        PZEM004T.__init__(self)
+    def __init__(self, host, port, slaves):
+        PZEM004T.__init__(self, slaves)
         self.port_name = host + ":" + str(port)
         self.modbus_client = ModbusTcpClient(host, port, ModbusRtuFramer)
